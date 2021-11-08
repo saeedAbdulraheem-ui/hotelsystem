9ab07e23f323b106ea4471564be953c10abb156f
Saeed <AbdulraheemSaeed3@gmail.com>
2021-11-08 22:58:54 +0200

    v11

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,16 +30,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         MainActivity.context = getApplicationContext();
         setContentView(R.layout.activity_main);
+        loadprevemail();
         addItemsOnSpinner1();
         addListenerOnButton();
         addListenerOnSpinnerItemSelection();
     }
     public void addItemsOnSpinner1() {
-
         spinner1 = (Spinner) findViewById(R.id.spinner1);
         List<String> list = new ArrayList<String>();
         list.add("BMI calculator");
@@ -52,13 +58,21 @@ public class MainActivity extends AppCompatActivity {
         spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
     }
     public void addListenerOnButton() {
-
+        EditText emailfield = (EditText) findViewById(R.id.editTextTextEmailAddress);
         spinner1 = (Spinner) findViewById(R.id.spinner1);
         btnSubmit = (Button) findViewById(R.id.enterAppButton);
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
         public void onClick(View v) {
             String selection = spinner1.getSelectedItem().toString();//check this item if BMI go to BMI entry otherwise go to other one
+            String email = emailfield.getText().toString();
+            if(checkemail(email)){ // if the email is real save it locally in json format
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                SharedPreferences.Editor editor = prefs.edit();
+                Gson gson = new Gson();
+                String emailjson = gson.toJson(email);
+                editor.putString("123", emailjson);
+                editor.commit();
                 if(selection.equalsIgnoreCase("BMI calculator")){
                     Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                     startActivity(intent);
@@ -70,7 +84,9 @@ public class MainActivity extends AppCompatActivity {
                             "Please select an Activity from the list",
                             Toast.LENGTH_SHORT).show();
                 }
-
+            }else{
+                Toast.makeText(MainActivity.this, "Invalid Email, check again", Toast.LENGTH_SHORT).show();
+            }
             Toast.makeText(MainActivity.this,
                     "OnClickListener : " +
                             "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()),
@@ -80,4 +96,28 @@ public class MainActivity extends AppCompatActivity {
 
     });
 
-}}
+    }
+
+    public void loadprevemail() {
+        EditText emailfield = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Gson gson = new Gson();
+        String str = prefs.getString("123", "");
+        String email = gson.fromJson(str, String.class);
+        if(email!= null) {
+            emailfield.setText(email);
+        }
+    }
+    public static boolean checkemail(String email){   //checks if a string is an email
+        Toast.makeText(getAppContext(), email,
+                Toast.LENGTH_SHORT).show();
+        Pattern pattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
+        Matcher mat = pattern.matcher(email);
+
+        if(mat.matches()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -6,6 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -14,6 +19,7 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         setupReturnButton();
+        calculateBmi();
     }
 
     public void setupReturnButton(){
@@ -23,6 +29,33 @@ public class MainActivity2 extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity2.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+    }
+
+    public void calculateBmi(){
+        Button getbmi = (Button)findViewById(R.id.calcbmi);
+        getbmi.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView bmires = (TextView)findViewById(R.id.resultofbmi);
+                EditText weightedit = (EditText)findViewById(R.id.editweight);
+                EditText heightedit = (EditText)findViewById(R.id.editheight);
+                float weight,height,bmi;
+                if(weightedit.getText().toString().length() > 1 && heightedit.getText().toString().length()>1) {
+                    weight = Float.parseFloat(weightedit.getText().toString());
+                    height = Float.parseFloat(heightedit.getText().toString()); //bmi = KG/M^2
+                    bmi = 1000*weight/(height*height);
+                    if(bmi > 25){
+                        //fat
+                        bmires.setText(bmi +" overweight");
+                    }else{
+                        //normal
+                        bmires.setText(bmi +" normal BMI");
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(),"please enter proper values",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
