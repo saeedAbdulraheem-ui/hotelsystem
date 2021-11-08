a066d6c2b1b0cb425793ae9d760947b36d220fdd
Saeed <AbdulraheemSaeed3@gmail.com>
2021-11-08 18:48:30 +0200

    v11

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        setupReturnButton();
+    }
+
+    public void setupReturnButton(){
+        Button returntolobby = (Button)findViewById(R.id.returnfrombmi);
+
+        returntolobby.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
